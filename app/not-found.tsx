cafe6e79428ac005f3bf07e98e30868806e6ffde
msi404 <ismail.merced3@gmail.com>
2025-03-09 02:43:54 +0300

    chore: update not found ui page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,20 @@
 import Link from 'next/link'
- import {Container} from '@/app/_components/containers/container'
+import {SearchX} from 'lucide-react'
+import { Container } from '@/app/_components/containers/container'
+ import {Button} from '@/app/_components/ui/button'
 export default function NotFound() {
   return (
-    <Container>
-      <h2>لا يوجد</h2>
-      <p>لا يمكن ايجاد الصفحة</p>
-      <Link href="/">الرجوع الى الصفحة الرئيسية</Link>
+    <Container className='mt-16 mx-auto flex justify-center items-center'>
+      <div className='flex flex-col items-center space-y-3'>
+      <SearchX className='size-64' color='gray'/>
+      <h1 className='text-4xl font-semibold text-gray-500'>لا يوجد</h1>
+      <p className='text-gray-500'>لا يمكن ايجاد الصفحة</p>
+        <Link href="/">
+          <Button variant='link'>
+        الرجوع الى الصفحة الرئيسية
+          </Button>
+      </Link>
+      </div>
     </Container>
   )
 }
